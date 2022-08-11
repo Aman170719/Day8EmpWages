4c1e72155fa56c6eef4ae916746cda0720471215
Aman <aman.kumar99499@gmail.com>
2022-08-11 14:01:40 +0530

    Added DailyWages

--- EmpWages/src/EmpWage.java
+++ EmpWages/src/EmpWage.java
@@ -1,20 +1,31 @@
 import java.util.Random;
 
 class EmployeeWages{
-    static Random random = new Random();
-    static int check;
-    public static void Attendance(){
+        static Random random = new Random();
+        static int check;
+        static int EmployeeWage = 20, workingHr, workingDays=20, EmployeeSalary =0;
+        public static void Attendance(){
         check = random.nextInt(3);
         if(check == 1){
             System.out.println("The Employee Is Present ");
         }else{
             System.out.println("The Employee Is Absent");
         }
-    }
-
-    public static void main(String[] args) {
+        }
+        public static void dailyWages() {
+        check = random.nextInt(3);
+        if(check == 1){
+            workingHr = 8;
+        }
+        else{
+            workingHr = 0;
+        }
+        EmployeeSalary =(workingHr*EmployeeWage);
+        System.out.println("Employee is Present and His/Her Daily Salary is " + EmployeeSalary);
+        }
+        public static void main(String[] args) {
         System.out.println("Welcome To The Employee Wages Salary Stimulator");
         EmployeeWages.Attendance();
-
-    }
-    }
+        EmployeeWages.dailyWages();
+        }
+        }
