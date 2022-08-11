671a31eb4173afb496f50f7569c320d2188a0872
Aman <aman.kumar99499@gmail.com>
2022-08-11 13:56:21 +0530

    Added Attendance

--- EmpWages/src/EmpWage.java
+++ EmpWages/src/EmpWage.java
@@ -3,7 +3,6 @@ import java.util.Random;
 class EmployeeWages{
     static Random random = new Random();
     static int check;
-    static int EmployeeWage = 20, workingHr, workingDays=20, EmployeeSalary =0;
     public static void Attendance(){
         check = random.nextInt(3);
         if(check == 1){
@@ -12,31 +11,10 @@ class EmployeeWages{
             System.out.println("The Employee Is Absent");
         }
     }
-    public static void dailyWages() {
-        check = random.nextInt(3);
-        if(check == 1){
-            workingHr = 8;
-            }
-        else{
-            workingHr = 0;
-        }
-        EmployeeSalary =(workingHr*EmployeeWage);
-        System.out.println("Employee is Present and His/Her Daily Salary is " + EmployeeSalary);
-    }
-    public static void monthlyWages(){
-        check = random.nextInt(3);
-        if(check == 1){
-            workingHr = 8;
-        }
-        else{
-            workingHr = 0;
-        }
-        EmployeeSalary =((workingHr*EmployeeWage)*workingDays);
-        System.out.println("Employee is Present and His/Her Monthly Salary is " + EmployeeSalary);
-    }
+
     public static void main(String[] args) {
+        System.out.println("Welcome To The Employee Wages Salary Stimulator");
         EmployeeWages.Attendance();
-        EmployeeWages.dailyWages();
-        EmployeeWages.monthlyWages();
+
     }
     }
