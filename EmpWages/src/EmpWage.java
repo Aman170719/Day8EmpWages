24a8e00702567b18c8535f7586e3961ebe8b151b
Aman <aman.kumar99499@gmail.com>
2022-08-11 22:23:43 +0530

    Added Monthly Wages

--- EmpWages/src/EmpWage.java
+++ EmpWages/src/EmpWage.java
@@ -27,21 +27,39 @@ class EmployeeWages{
         check = random.nextInt(3);
         if(check == 1){
                 workingHr = 8;
+                System.out.println("Employee is Present and His/Her Monthly Salary is " + EmployeeSalary);
                 }
         else if(check ==2){
                 workingHr = 4;
+                System.out.println("Employee is Present and His/Her Monthly Salary is " + EmployeeSalary);
                 }
         else {
                 workingHr=0;
                 }
+                System.out.println("Employee is Absent");
         EmployeeSalary =((workingHr*EmployeeWage)*workingDays);
-        System.out.println("Employee is Present and His/Her Monthly Salary is " + EmployeeSalary);
+
         }
-      
+        public static void switchCase(){
+                switch(check){
+                        case 1: workingHr =8;
+                                EmployeeSalary =((workingHr*EmployeeWage)*workingDays);
+                        System.out.println("S/C Employee is Present and His/Her Monthly Salary is " + EmployeeSalary);
+                        break;
+                        case 2: workingHr = 4;
+                                EmployeeSalary =((workingHr*EmployeeWage)*workingDays);
+                                System.out.println("S/C Employee is Present and His/Her Monthly Salary is " + EmployeeSalary);
+                        break;
+                        default: workingHr=0;
+                                System.out.println("S/C Employee is Absent");
+                        break;
+                        }
+                }
         public static void main(String[] args) {
         System.out.println("Welcome To The Employee Wages Salary Stimulator");
         EmployeeWages.Attendance();
         EmployeeWages.dailyWages();
         EmployeeWages.monthlyWages();
+        EmployeeWages.switchCase();
         }
         }
